249bef394f0c68bdcb485175efe08ec32b48cca0
Franco Alarcon <franco.alarcon@attackiq.com>
2020-04-09 10:31:42 -0300

    Completes section 3

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,13 +3,14 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService:TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTaskWithFilters(filterDto);
     }
@@ -36,7 +37,9 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  updateStatus(@Param('id') id: string, @Body('status') status: TaskStatus) {
+  updateStatus(
+    @Param('id') id: string, 
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus) {
     return this.tasksService.updateStatus(id, status);
   }
 }
